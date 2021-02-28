94b99bf9fb7309261b254529ffdf7b47cf72de0b
Bader-eng <baderali2016eng@gmail.com>
2021-02-28 22:56:31 +0200

    change

---
+++ Problem_Domain/js/app.js
@@ -0,0 +1,17 @@
+
+let username=prompt('enter your name:');
+console.log(username);
+document.write("<h3> user name    :"  + username + "</h3>")
+let userstyle=prompt('what your Fashion:dresses:one shoulder,off shoulder,backless ');
+console.log(userstyle);
+document.write("<h3> user style    :"  + userstyle + "</h3>")
+
+let usersize=prompt('what your size:UK Size or US Size');
+console.log(usersize);
+document.write(" <h3> user size    :"  + usersize + "</h3>")
+
+let usercolor=prompt('what your fav color');
+console.log(usercolor);
+document.write("<h3> user color    :"  + usercolor + "<h3>")
+ alert("Hello" +    username        + "nice to meet you are like"  +    userstyle    +   "style and your fav color is"   + usercolor +    "and you have nice body with size"    +   usersize   +   ",we will help you");
+ 
